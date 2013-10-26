8619a8c2dd4ba8722533cdc00b00ac8711825c55
BlueN <github@imorz.tk>
2013-10-26 22:04:03 +0800

    change broadcast to wifi.STATE_CHANGE

--- src/org/xierch/mpiwifi/NetworkBroadcast.java
+++ src/org/xierch/mpiwifi/NetworkBroadcast.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
-import android.net.ConnectivityManager;
 import android.net.wifi.WifiInfo;  
 import android.net.wifi.WifiManager;  
 
@@ -15,34 +14,37 @@ public class NetworkBroadcast extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (!intent.getAction().equals
-				(ConnectivityManager.CONNECTIVITY_ACTION))
+				(WifiManager.NETWORK_STATE_CHANGED_ACTION))
 			return;
+		if (! intent.getExtras().containsKey(WifiManager.EXTRA_WIFI_INFO))
+			return;  //  The new state is not CONNECTED.
+		WifiInfo wifiInfo = (WifiInfo) intent.getExtras().get(WifiManager.EXTRA_WIFI_INFO);
+
+		String target;
+		String ssid = wifiInfo.getSSID();
+		if (ssid == null)
+			return;
+		else if (ssid.equals("NamOn_Hostel"))
+			target = "Namon";
+		else
+			return;
+
     	SharedPreferences settings = context
     			.getSharedPreferences("settings", Context.MODE_PRIVATE);
     	if (!settings.getBoolean("autoLogin", false)) return;
-    	
+
     	SharedPreferences loginInfo = context
     			.getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
     	String netId = loginInfo.getString("netId", "");
     	String pwd   = loginInfo.getString("pwd", "");
-    	
+
     	if (netId.isEmpty() || pwd.isEmpty()) {
     		SharedPreferences.Editor editor = settings.edit();
     		editor.putBoolean("autoLogin", false);
     		editor.commit();
     		return;
     	}
-    	
-    	WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-    	if (!wifiManager.isWifiEnabled()) return;
-    	WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-    	if (wifiInfo == null) return;
-    	String ssid = wifiInfo.getSSID();
-    	String target;
-    	if (ssid == null) return;
-    	if (wifiInfo.getSSID().equals("NamOn_Hostel")) target = "Namon";
-    	else return;
-    	
+
     	Intent login = new Intent(context, WifiLoginer.class);
     	login.putExtra("target", target);
     	login.putExtra("username", netId);
