8bd34161d5146209616944f0137bdda4a5b7d632
BlueN <orz@sorz.org>
2013-11-15 22:05:22 +0800

    Reformat Java code

--- src/org/xierch/mpiwifi/NetworkBroadcast.java
+++ src/org/xierch/mpiwifi/NetworkBroadcast.java
@@ -5,20 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
-import android.net.wifi.WifiInfo;  
-import android.net.wifi.WifiManager;  
-
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 
 public class NetworkBroadcast extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (!intent.getAction().equals
-				(WifiManager.NETWORK_STATE_CHANGED_ACTION))
+		if (!intent.getAction()
+				.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION))
 			return;
-		if (! intent.getExtras().containsKey(WifiManager.EXTRA_WIFI_INFO))
-			return;  //  The new state is not CONNECTED.
-		WifiInfo wifiInfo = (WifiInfo) intent.getExtras().get(WifiManager.EXTRA_WIFI_INFO);
+		if (!intent.getExtras().containsKey(WifiManager.EXTRA_WIFI_INFO))
+			return; // The new state is not CONNECTED.
+		WifiInfo wifiInfo = (WifiInfo) intent.getExtras().get(
+				WifiManager.EXTRA_WIFI_INFO);
 
 		String target;
 		String ssid = wifiInfo.getSSID();
@@ -29,28 +29,29 @@ public class NetworkBroadcast extends BroadcastReceiver {
 		else
 			return;
 
-    	SharedPreferences settings = context
-    			.getSharedPreferences("settings", Context.MODE_PRIVATE);
-    	if (!settings.getBoolean("autoLogin", false)) return;
+		SharedPreferences settings = context.getSharedPreferences("settings",
+				Context.MODE_PRIVATE);
+		if (!settings.getBoolean("autoLogin", false))
+			return;
 
-    	SharedPreferences loginInfo = context
-    			.getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
-    	String netId = loginInfo.getString("netId", "");
-    	String pwd   = loginInfo.getString("pwd", "");
+		SharedPreferences loginInfo = context.getSharedPreferences("loginInfo",
+				Context.MODE_PRIVATE);
+		String netId = loginInfo.getString("netId", "");
+		String pwd = loginInfo.getString("pwd", "");
 
-    	if (netId.isEmpty() || pwd.isEmpty()) {
-    		SharedPreferences.Editor editor = settings.edit();
-    		editor.putBoolean("autoLogin", false);
-    		editor.commit();
-    		return;
-    	}
+		if (netId.isEmpty() || pwd.isEmpty()) {
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putBoolean("autoLogin", false);
+			editor.commit();
+			return;
+		}
 
-    	Intent login = new Intent(context, WifiLoginer.class);
-    	login.putExtra("target", target);
-    	login.putExtra("username", netId);
-    	login.putExtra("password", pwd);
-    	login.putExtra("lessToast", true);
-    	context.startService(login);
+		Intent login = new Intent(context, WifiLoginer.class);
+		login.putExtra("target", target);
+		login.putExtra("username", netId);
+		login.putExtra("password", pwd);
+		login.putExtra("lessToast", true);
+		context.startService(login);
 	}
 
 }
